9f06a7c64e2740b9e75f4bad502df7859d866a7b
Link <1910450802@qq.com>
2020-06-13 23:46:21 +0800

    稍稍修改

--- 数据库和源码1/码源/LightTimeBlog/LightTimeBlog/Controllers/LoginController.cs
+++ 数据库和源码1/码源/LightTimeBlog/LightTimeBlog/Controllers/LoginController.cs
@@ -9,9 +9,15 @@ namespace LightTimeBlog.Controllers
     public class LoginController : Controller
     {
         // GET: Login
+        
         public ActionResult Login()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string email)
+        {
+            return RedirectToAction("Index", "Home");
         }
     }
 }
